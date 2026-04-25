0dd50722f7908931a04d7b4f443a127d42a3e70f
amelienorris <aminrs0315@gmail.com>
2026-04-24 20:58:48 -0700

    Added data handling (insert, get, update, delete for tasks) for other data tables

--- src/main/java/database/DatabaseManager.java
+++ src/main/java/database/DatabaseManager.java
@@ -1,6 +1,9 @@
 package database;
 import java.sql.*;
 import database.User;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseManager {
   private static final String DB_URL = "jdbc:squlite:MainApp.db";
   private static DatabaseManager instance; // make an instance for singleton
@@ -53,6 +56,7 @@ public class DatabaseManager {
           is_completed INTEGER DEFAULT 0,
           created_at TEXT DEFAULT (datetime('now')),
           FOREIGN KEY (user_id) REFERENCES users(user_id)
+          
           """;
       String focus = """
           CREATE TABLE IF NOT EXISTS focus_sessions(
@@ -62,9 +66,9 @@ public class DatabaseManager {
               duration_minutes INTEGER,
               completed INTEGER DEFAULT 0,
               session_date TEXT DEFAULT(datetime('now')),
-              FORIEGN KEY (user_id) REFERENCES users(user_id),
-              FOREIGN KEY (task_id) REFERENCES (tasks(task_id))
-          )""";
+              FOREIGN KEY (user_id) REFERENCES users(user_id),
+              FOREIGN KEY (task_id) REFERENCES tasks(task_id)
+          """;
       stmt.execute(users);
       stmt.execute(tasks);
       stmt.execute(focus);
@@ -124,5 +128,74 @@ public class DatabaseManager {
     } catch (SQLException e){
       System.err.println("delete failed: " + e.getMessage());
     }
+  }
+  public void insertTask(int userId, String title, String description, String due, String priority){
+    String sql = "INSERT INTO tasks(user_id, title, description, due_date, priority) VALUES (?,?,?,?,?)";
+    try (PreparedStatement pstmt = connection.prepareStatement(sql)){
+      pstmt.setInt(1, userId);
+      pstmt.setString(2, title);
+      pstmt.setString(3, description);
+      pstmt.setString(4, due);
+      pstmt.setString(5, priority);
+    } catch (SQLException e){
+      System.err.println("add task failed " + e.getMessage());
+    }
+  }
+  public List<String> getTasks(int userId){
+    List<String> tasks = new ArrayList<>();
+    String sql = "SELECT title FROM tasks WHERE user_id = ? AND is_completed = 0 ORDER BY due_date";
+    try(PreparedStatement pstmt = connection.prepareStatement(sql)){
+      pstmt.setInt(1, userId);
+      ResultSet rs = pstmt.executeQuery();
+      while(rs.next()){
+        tasks.add(rs.getString("title"));
+      }
+    } catch (SQLException e){
+      System.err.println("Failed to get tasks " + e.getMessage());
+    }
+    return tasks;
+  }
+  public void completeTask(int taskId){
+    String sql = "UPDATE tasks SET is_completed = 1 WHERE task_id = ?";
+    try(PreparedStatement pstmt = connection.prepareStatement(sql)){
+      pstmt.setInt(1, taskId);
+      pstmt.executeUpdate();
+    } catch(SQLException e){
+      System.err.println("could not mark completed " + e.getMessage());
+    }
+  }
+  public void deleteTask(int taskId){
+    String sql = "DELTE FROM tasks WHERE task_id = ?";
+    try(PreparedStatement pstmt = connection.prepareStatement(sql)){
+      pstmt.setInt(1, taskId);
+      pstmt.executeUpdate();
+    } catch(SQLException e){
+      System.err.println("could not delete" + e.getMessage());
+    }
+  }
+  public void insertFocus(int userId, int taskId, int duration, boolean completed){
+    String sql = "INSERT INTO focus_sessions(user_id, task_id, duration_minutes, completed) VALUES (?,?,?,?)";
+    try (PreparedStatement pstmt = connection.prepareStatement(sql)){
+      pstmt.setInt(1, userId);
+      pstmt.setInt(2, taskId);
+      pstmt.setInt(3, duration);
+      pstmt.setInt(4, completed ? 1 : 0);
+    } catch (SQLException e){
+      System.err.println("add task failed " + e.getMessage());
+    }
+  }
+  public int getSesssionCount(int userId){
+    int total = 0;
+    String sql = "SELECT session_id FROM focus_sessions WHERE user_id = ? AND completed = 1";
+    try(PreparedStatement pstmt = connection.prepareStatement(sql)){
+      pstmt.setInt(1, userId);
+      ResultSet rs = pstmt.executeQuery();
+      while(rs.next()){
+        total++;
+      }
+    } catch (SQLException e){
+      System.err.println("Failed to get tasks " + e.getMessage());
+    }
+    return total;
   }
 }
