79d90702b2f5212f1f61643af702a3d45f6e96e0
Moises D. <161376587+bokehluv@users.noreply.github.com>
2026-04-26 12:34:58 -0700

    change main to use singleton

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,16 +9,14 @@ public class Main extends Application {
   private DatabaseManager db ;
   @Override
   public void start (Stage stage) {
-    db = new DatabaseManager () ; // opens / creates app.db
-
+    SceneManager.init(stage);
     stage.setTitle("Todo App");
-    SceneManager.init(stage, db);
     SceneManager.getInstance().navigateTo(SceneType.WELCOME);
-    stage.show () ;
+    stage.show();
 
   }
   @Override
   public void stop () throws SQLException {
-    if ( db != null ) db.close() ; // called automatically on window close
-  } // framework for SQLite database implementation with scenemanger, to be implemented fully after we work on scenefactory
+    DatabaseManager.getInstance().close();
+  }
 }
