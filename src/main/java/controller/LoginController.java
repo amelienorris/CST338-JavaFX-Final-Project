ef60398f8dd214c0effaa2c17c20fe532bf3fb09
amelienorris <aminrs0315@gmail.com>
2026-04-27 16:39:35 -0700

    Login handling, still needs redirect logic

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,5 +1,29 @@
 package controller;
+import database.DatabaseManager;
+import database.User;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 
 public class LoginController {
+  @FXML
+  private TextField usernameField;
+  @FXML
+  private PasswordField passwordField;
+  @FXML
+  private Label error;
+
+  @FXML
+  private void Login(){
+    DatabaseManager db = DatabaseManager.getInstance();
+    String user = usernameField.getText();
+    String pw = passwordField.getText();
+    if(db.getUser(user, pw) == null){
+      error.setText("Incorrect Login.");
+      usernameField.clear();
+      passwordField.clear();
+      return;
+    }
+  }
 
 }
