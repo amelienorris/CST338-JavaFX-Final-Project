f274e8b4575fe2db7a8eba658694349e612e7ca5
amikaella <amikaellachua@gmail.com>
2026-05-03 21:24:43 -0700

    fixed typos and sapcing

--- src/main/java/controller/TaskListController.java
+++ src/main/java/controller/TaskListController.java
@@ -416,7 +416,7 @@ public class TaskListController {
             Alert completed_alert = new Alert(Alert.AlertType.INFORMATION);
             completed_alert.setTitle("Task Completed");
             completed_alert.setHeaderText(null);
-            completed_alert.setContentText("Congratulation! Task has been marked as completed");
+            completed_alert.setContentText("Congratulationa! Task has been marked as completed");
             completed_alert.showAndWait();
         }
     }
